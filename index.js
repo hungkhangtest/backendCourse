48e10a4e99eab4d2a907f632473d48eb864f7083
hungkhangtest <153441163+hungkhangtest@users.noreply.github.com>
2023-12-10 22:20:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"https://courseacademy.vercel.app/",
+		origin:"https://courseacademy.vercel.app",
 		credentials:true,
 	})
 )
